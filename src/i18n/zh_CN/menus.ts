7a7ed5681c238597f9cac6ea2ebd756180f43323
MiyueSC <913784771@qq.com>
2024-02-05 16:41:03 +0800

    feat: i18n

--- src/i18n/zh_CN/menus.ts
+++ src/i18n/zh_CN/menus.ts
@@ -7,14 +7,21 @@ export default {
   HighlightTextModule: '高亮文本',
   FlowDesignModule: '钉钉流程',
   OrganizationTreeModule: '组织树',
+  VirtualListModule: '虚拟列表',
+  TaskDashboardModule: '任务看板',
+  EchartsExModule: 'Echarts图表集',
   //
   CssAnimation: 'CSS 动画效果',
   SimpleSpinLoading: 'CSS 简易加载动画',
   ComplexSpinLoading: 'CSS 复杂加载动画',
   ScssSnowBg: 'Scss 落雪效果',
+  ButtonHover: '按钮 Hover 效果',
+  ThreeDotsLoading: '三点式加载动画',
   //
   JSAnimation: 'JS 动画效果',
   MouseMoveAnimate: '鼠标滑动偏移',
+  SplittingImageCover: '图片分割动画',
+  Transform3D: '3D 行星环绕',
   //
   ExcellentResources: '资源列表',
   CssSources: 'CSS 资源库',
