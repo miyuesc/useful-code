0d7f589c0a53e0400617184e57f0cce28def76b4
MiyueSC <913784771@qq.com>
2024-05-13 17:05:22 +0800

    1

---
+++ src/components/ExpressionGenerator/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  defineOptions({ name: 'ExpressionGenerator' })
+</script>
+
+<template>
+  <div class="mi-expression-generator"></div>
+</template>
+
+<style scoped lang="scss"></style>
--- src/functions/EventEmitter.ts
+++ src/functions/EventEmitter.ts
@@ -3,11 +3,13 @@ import { notNull, isArray } from './tool'
 const isNullOrUndefined = (obj: unknown) => !notNull(obj)
 
 class EventEmitter {
-  static _events: { [key: string]: Function[] } = {}
+  private _events: { [p: string]: Function[] }
 
-  constructor() {}
+  constructor() {
+    this._events = {}
+  }
 
-  static _addListener(type: string, fn: any, context?: any, once?: boolean) {
+  _addListener(type: string, fn: any, context?: any, once?: boolean) {
     if (typeof fn !== 'function') {
       throw new TypeError('fn must be a function')
     }
@@ -15,60 +17,56 @@ class EventEmitter {
     fn.context = context
     fn.once = !!once
 
-    const event = EventEmitter._events[type]
+    const event = this._events[type]
     // only one, let `this._events[type]` to be a function
     if (isNullOrUndefined(event)) {
-      EventEmitter._events[type] = fn
+      this._events[type] = fn
     } else if (typeof event === 'function') {
       // already has one function, `this._events[type]` must be a function before
-      EventEmitter._events[type] = [event, fn]
+      this._events[type] = [event, fn]
     } else if (isArray(event)) {
       // already has more than one function, just push
-      EventEmitter._events[type].push(fn)
+      this._events[type].push(fn)
     }
 
-    return EventEmitter
+    return this
   }
 
-  static addListener(type: string, fn: Function, context?: any) {
-    return EventEmitter._addListener(type, fn, context)
+  addListener(type: string, fn: Function, context?: any) {
+    return this._addListener(type, fn, context)
   }
 
-  static hasListener(type: string, fn: Function) {
-    return (
-      EventEmitter._events[type] &&
-      EventEmitter._events[type].indexOf &&
-      EventEmitter._events[type].indexOf(fn) > -1
-    )
+  hasListener(type: string, fn: Function) {
+    return this._events[type] && this._events[type].indexOf && this._events[type].indexOf(fn) > -1
   }
 
-  static on(type: string, fn: any, context?: any) {
-    return EventEmitter.addListener(type, fn, context)
+  on(type: string, fn: any, context?: any) {
+    return this.addListener(type, fn, context)
   }
 
-  static once(type: string, fn: any, context?: any) {
-    return EventEmitter._addListener(type, fn, context, true)
+  once(type: string, fn: any, context?: any) {
+    return this._addListener(type, fn, context, true)
   }
 
-  static emit(type: string, ...rest: any[]) {
+  emit(type: string, ...rest: any[]) {
     if (isNullOrUndefined(type)) {
       throw new Error('emit must receive at lease one argument')
     }
 
-    const event: any = EventEmitter._events[type]
+    const event: any = this._events[type]
 
     if (isNullOrUndefined(event)) return false
 
     if (typeof event === 'function') {
       event.call(event.context || null, ...rest)
       if (event.once) {
-        EventEmitter.removeListener(type, event)
+        this.removeListener(type, event)
       }
     } else if (isArray(event)) {
       event.map((e) => {
         e.call(e.context || null, ...rest)
         if (e.once) {
-          EventEmitter.removeListener(type, e)
+          this.removeListener(type, e)
         }
       })
     }
@@ -76,27 +74,27 @@ class EventEmitter {
     return true
   }
 
-  static removeListener(type: string, fn: Function) {
-    if (isNullOrUndefined(EventEmitter._events)) return EventEmitter
+  removeListener(type: string, fn: Function) {
+    if (isNullOrUndefined(this._events)) return this
 
     // if type is undefined or null, nothing to do, just return this
-    if (isNullOrUndefined(type)) return EventEmitter
+    if (isNullOrUndefined(type)) return this
 
     if (typeof fn !== 'function') {
       throw new Error('fn must be a function')
     }
 
-    const events = EventEmitter._events[type]
+    const events = this._events[type]
     if (!events) {
-      return EventEmitter
+      return this
     }
 
     if (typeof events === 'function') {
-      events === fn && delete EventEmitter._events[type]
+      events === fn && delete this._events[type]
     } else {
       const findIndex = events.findIndex((e) => e === fn)
 
-      if (findIndex === -1) return EventEmitter
+      if (findIndex === -1) return this
 
       // match the first one, shift faster than splice
       if (findIndex === 0) {
@@ -108,38 +106,38 @@ class EventEmitter {
       // just left one listener, change Array to Function
       if (events.length === 1) {
         // @ts-ignore
-        EventEmitter._events[type] = events[0]
+        this._events[type] = events[0]
       }
     }
 
-    return EventEmitter
+    return this
   }
 
-  static removeAllListeners(type?: string) {
-    if (isNullOrUndefined(EventEmitter._events)) return EventEmitter
+  removeAllListeners(type?: string) {
+    if (isNullOrUndefined(this._events)) return this
 
     // if not provide type, remove all
     if (isNullOrUndefined(type)) {
-      return (EventEmitter._events = Object.create(null))
+      return (this._events = Object.create(null))
     }
 
-    const events = EventEmitter._events[type!]
+    const events = this._events[type!]
     if (!isNullOrUndefined(events)) {
       // check if `type` is the last one
-      if (Object.keys(EventEmitter._events).length === 1) {
-        EventEmitter._events = Object.create(null)
+      if (Object.keys(this._events).length === 1) {
+        this._events = Object.create(null)
       } else {
-        delete EventEmitter._events[type!]
+        delete this._events[type!]
       }
     }
 
-    return EventEmitter
+    return this
   }
 
-  static listeners(type: string) {
-    if (isNullOrUndefined(EventEmitter._events)) return []
+  listeners(type: string) {
+    if (isNullOrUndefined(this._events)) return []
 
-    const events = EventEmitter._events[type]
+    const events = this._events[type]
     // use `map` because we need to return a new array
     return isNullOrUndefined(events)
       ? []
@@ -148,18 +146,18 @@ class EventEmitter {
       : events.map((o) => o)
   }
 
-  static listenerCount(type: string) {
-    if (isNullOrUndefined(EventEmitter._events)) return 0
+  listenerCount(type: string) {
+    if (isNullOrUndefined(this._events)) return 0
 
-    const events = EventEmitter._events[type]
+    const events = this._events[type]
 
     return isNullOrUndefined(events) ? 0 : typeof events === 'function' ? 1 : events.length
   }
 
-  static eventNames() {
-    if (isNullOrUndefined(EventEmitter._events)) return []
+  eventNames() {
+    if (isNullOrUndefined(this._events)) return []
 
-    return Object.keys(EventEmitter._events)
+    return Object.keys(this._events)
   }
 }
 
